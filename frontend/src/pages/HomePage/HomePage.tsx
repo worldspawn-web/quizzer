36e3bc92215ecc65082bd836bcdfcbd4003865a1
Worldspawn <worldspawn.web@gmail.com>
2024-02-15 17:56:05 +0300

    feat: HomePage (mobile version)

---
+++ frontend/src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,33 @@
+import { RenderQuizzes } from '../../common';
+import styles from './HomePage.module.scss';
+
+export const HomePage = () => {
+  return (
+    <div className={styles.page}>
+      <header className={styles.header__wrapper}>
+        <h1>Quizzer</h1>
+      </header>
+      <section className={styles.page__quizzes}>
+        <div className={styles.page__quizzes__hot}>
+          <span>Hot!🔥</span>
+          <div className={styles.quizzes__wrapper}>
+            <RenderQuizzes />
+          </div>
+        </div>
+        <div className={styles.page__quizzes__new}>
+          New Quizzes👻
+          <div className={styles.quizzes__wrapper}>
+            <RenderQuizzes />
+          </div>
+        </div>
+        <div className={styles.page__quizzes__updated}>
+          Recently Updated 🔃
+          <div className={styles.quizzes__wrapper}>
+            <RenderQuizzes />
+          </div>
+        </div>
+      </section>
+      <footer></footer>
+    </div>
+  );
+};
