15fd00dbf0d5452eb415476f5c3dbb7da96f2df4
Worldspawn <worldspawn.web@gmail.com>
2024-02-19 17:34:44 +0300

    feat: router for test quiz

--- frontend/src/router/routenames.ts
+++ frontend/src/router/routenames.ts
@@ -2,4 +2,5 @@ export const routenames = {
   HOME: '/',
   NOT_FOUND: '*',
   QUIZ_PAGE: '/quiz/:id',
+  QUIZ_TEST: '/test/:id',
 };
--- frontend/src/router/router.tsx
+++ frontend/src/router/router.tsx
@@ -5,10 +5,12 @@ import { routenames } from './routenames';
 import { HomePage } from '../pages';
 
 const QuizPage = lazy(() => import('../pages/Quiz/Quiz'));
+const TestPage = lazy(() => import('../pages/Test/Test'));
 
 export const Router: FC = () => (
   <Routes>
     <Route path={routenames.HOME} element={<HomePage />} />
     <Route path={routenames.QUIZ_PAGE} element={<QuizPage />} />
+    <Route path={routenames.QUIZ_TEST} element={<TestPage />} />
   </Routes>
 );
